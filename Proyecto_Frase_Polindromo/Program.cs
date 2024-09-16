94ff9d73548ad77c4d266cdfbaa19041c6044dbc
amax2019189 <amax-2019189@kinal.edu.gt>
2024-09-16 09:48:52 -0600

    Se cargo el proyecto propuesto frase polindromo

---
+++ Proyecto_Frase_Polindromo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FrasePalindromo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Por favor agrege una frase: ");
+            String frase = Console.ReadLine().Replace(" ", "");
+
+            string fraseLimpia = new string(frase
+                .ToLower()
+                .Where(c => char.IsLetter(c))
+                .ToArray());
+
+            String resultado = InvertirPalabra(frase);
+
+            if (frase == resultado)
+            {
+                Console.WriteLine("La palabra " + resultado + " si es palíndromo");
+            }
+            else
+            {
+                Console.WriteLine("La palabra " + resultado + " no es palíndromo");
+            }
+
+            Console.ReadLine();
+        }
+
+        static string InvertirPalabra(string frase)
+        {
+            char[] caracteres = frase.ToCharArray();
+            Array.Reverse(caracteres);
+            return new string(caracteres);
+        }
+    }
+}
