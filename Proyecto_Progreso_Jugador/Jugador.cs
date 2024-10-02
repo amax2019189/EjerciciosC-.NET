5f87e7a439391d219d8028a90348f62bd9e6620d
amax2019189 <amax-2019189@kinal.edu.gt>
2024-10-02 10:20:37 -0600

    Se cargo el proyecto propuesto para la semana

---
+++ Proyecto_Progreso_Jugador/Jugador.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemana5
+{
+    [Serializable]
+    public class Jugador
+    {
+        public string Nombre { get; set; }
+        public int Nivel { get; set; }
+        public int PuntosDeVida { get; set; }
+        public int NumeroDeReinicios { get; set; }
+
+        public Jugador (string nombre, int nivel, int puntosDeVida, int numeroDeReinicios)
+        {
+            Nombre = nombre;
+            Nivel = nivel;
+            PuntosDeVida = puntosDeVida;
+            NumeroDeReinicios = numeroDeReinicios;
+        }
+
+        public void GuardarProgreso(string carpeta)
+        {
+            try
+            {
+                string rutaArchivo = Path.Combine(carpeta, $"{Nombre}.dat");
+
+                if (!Directory.Exists(carpeta))
+                {
+                    Directory.CreateDirectory(carpeta);
+                }
+
+                using (FileStream fs = new FileStream(rutaArchivo, FileMode.Create))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    formatter.Serialize(fs, this);
+                    Console.WriteLine("");
+                    Console.WriteLine($" Progreso de {Nombre} guardado correctamente");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($" Error al guardar el progreso de {Nombre}.");
+                RegistrarError(e);
+            }
+        }
+
+        public static Jugador CargarProgreso(string rutaArchivo)
+        {
+            try
+            {
+                using (FileStream fs = new FileStream(rutaArchivo, FileMode.Open))
+                {
+                    BinaryFormatter formatter = new BinaryFormatter();
+                    Jugador jugador = (Jugador)formatter.Deserialize(fs);
+                    Console.WriteLine(" Progreso cargado correctamente");
+                    return jugador;
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(" Error al cargar el progreso");
+                RegistrarError(e);
+                return null;
+            }
+        }
+
+        public static List<Jugador> CargarTodosLosJugadores(string carpeta)
+        {
+            List<Jugador> jugadores = new List<Jugador>();
+
+            try
+            {
+                if (Directory.Exists(carpeta))
+                {
+                    string[] archivos = Directory.GetFiles(carpeta, "*.dat");
+
+                    foreach (string archivo in archivos)
+                    {
+                        using (FileStream fs = new FileStream(archivo, FileMode.Open))
+                        {
+                            BinaryFormatter formatter = new BinaryFormatter();
+                            Jugador jugador = (Jugador)formatter.Deserialize(fs);
+                            jugadores.Add(jugador);
+                        }
+                    }
+
+                    Console.WriteLine($" {jugadores.Count} jugadores cargados exitosamente");
+                }
+                else
+                {
+                    Console.WriteLine(" La carpeta no existe");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(" Error al cargar los jugadores");
+                RegistrarError(ex);
+            }
+
+            return jugadores;
+        }
+
+        private static void RegistrarError(Exception e)
+        {
+            Console.WriteLine("Registrando error...");
+            string rutaLog = "recolector_errores.txt";
+            using (StreamWriter sw = new StreamWriter(rutaLog, true))
+            {
+                sw.WriteLine($"{DateTime.Now}: {e.Message}");
+                sw.WriteLine(e.StackTrace);
+            }
+        }
+    }
+}
---
+++ Proyecto_Progreso_Jugador/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemana5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string carpeta = "JugadoresGuardados";
+
+            List<Jugador> jugadores = new List<Jugador>
+            {
+                new Jugador("Alejandro", 15, 100, 2),
+                new Jugador("Benjamin", 20, 120, 3),
+                new Jugador("Max", 10, 80, 1),
+                new Jugador("Lopez", 25, 150, 4)
+            };
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write(" Quieres agregar un nuevo Jugador (S/N): ");
+            Console.ResetColor();
+            string respuesta = Console.ReadLine().ToLower();
+
+            while (respuesta == "s")
+            {
+                Console.WriteLine("");
+                Console.Write(" Ingrese el Nombre del jugador: ");
+                string nombre = Console.ReadLine();
+
+                Jugador jugadorExistente = jugadores.FirstOrDefault(j => j.Nombre == nombre);
+
+                if (jugadorExistente != null)
+                {
+                    Console.WriteLine("");
+                    Console.Write(" Ya existe un jugador con ese nombre. Desea editar (E) o crear un nuevo jugador (N): ");
+                    string accion = Console.ReadLine().ToLower();
+
+                    if (accion == "e")
+                    {
+                        Console.Write(" Ingrese el Nivel del jugador: ");
+                        jugadorExistente.Nivel = int.Parse(Console.ReadLine());
+
+                        Console.Write(" Ingrese los Puntos de Vida del jugador: ");
+                        jugadorExistente.PuntosDeVida = int.Parse(Console.ReadLine());
+
+                        Console.Write(" Ingrese el Numero de Reinicios del jugador: ");
+                        jugadorExistente.NumeroDeReinicios = int.Parse(Console.ReadLine());
+
+                        jugadorExistente.GuardarProgreso(carpeta);
+                        Console.WriteLine(" Jugador editado exitosamente");
+                    }
+                    else if (accion == "n")
+                    {
+                        Console.WriteLine("");
+                        Console.WriteLine(" Por favor, ingrese un nombre diferente para el nuevo jugador");
+                        Console.Write(" Ingrese un nuevo nombre de jugador: ");
+                        nombre = Console.ReadLine();
+
+                        Console.Write(" Ingrese el Nivel del jugador: ");
+                        int nivel = int.Parse(Console.ReadLine());
+
+                        Console.Write(" Ingrese los Puntos de Vida del jugador: ");
+                        int puntosDeVida = int.Parse(Console.ReadLine());
+
+                        Console.Write(" Ingrese el Numero de Reinicios del jugador: ");
+                        int numerDeReinicios = int.Parse(Console.ReadLine());
+
+                        Jugador nuevoJugador = new Jugador(nombre, nivel, puntosDeVida, numerDeReinicios);
+                        jugadores.Add(nuevoJugador);
+                        nuevoJugador.GuardarProgreso(carpeta);
+                    }
+                }
+                else
+                {
+                    Console.Write(" Ingrese el Nivel del jugador: ");
+                    int nivel = int.Parse(Console.ReadLine());
+
+                    Console.Write(" Ingrese los Puntos de Vida del jugador: ");
+                    int puntosDeVida = int.Parse(Console.ReadLine());
+
+                    Console.Write(" Ingrese el Numero de Reinicios del jugador: ");
+                    int numeroDeReinicios = int.Parse(Console.ReadLine());
+
+                    Jugador nuevoJugador = new Jugador(nombre, nivel, puntosDeVida, numeroDeReinicios);
+                    jugadores.Add(nuevoJugador);
+                    nuevoJugador.GuardarProgreso(carpeta);
+                    Console.WriteLine(" Jugador agregado exitosamente");
+                }
+
+                Console.WriteLine(" Jugador agregado Exitosamente");
+                Console.WriteLine("");
+                Console.Write(" Quieres agregar un nuevo Jugador (S/N): ");
+                respuesta = Console.ReadLine().ToLower();
+            }
+
+            Console.WriteLine("");
+            List<Jugador> jugadoresCargados = Jugador.CargarTodosLosJugadores(carpeta);
+            
+            Console.WriteLine("");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(" | JUGADORES GUARDADOS |");
+            Console.ResetColor();
+            Console.WriteLine("");
+
+            foreach (var jugador in jugadoresCargados)
+            {
+                Console.WriteLine($" Jugador: {jugador.Nombre}");
+                Console.WriteLine($" Nivel: {jugador.Nivel}");
+                Console.WriteLine($" Puntos de Vida: {jugador.PuntosDeVida}");
+                Console.WriteLine($" Número de Reinicios: {jugador.NumeroDeReinicios}\n");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
