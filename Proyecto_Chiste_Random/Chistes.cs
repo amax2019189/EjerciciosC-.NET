86f13770585a974c16e9ceb2fb4e6bc31848cc9f
amax2019189 <amax-2019189@kinal.edu.gt>
2024-10-08 20:38:52 -0600

    Se cargo el proyecto propuesto para esta semana

---
+++ Proyecto_Chiste_Random/Chistes.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace ProyectoSemana6
+{
+    class Chistes
+    {
+        private readonly string _filePath;
+
+        public Chistes(string filePath)
+        {
+            _filePath = filePath;
+            VerificarArchivo();
+        }
+
+        public void VerificarArchivo()
+        {
+            if (!File.Exists(_filePath))
+            {
+                using (File.Create(_filePath))
+                {
+                }
+                Console.WriteLine("Archivo creado: " + _filePath);
+            }
+        }
+
+        public async Task<string> ChisteAleatorio()
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync("https://api.chucknorris.io/jokes/random");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                var chiste = JsonConvert.DeserializeObject<dynamic>(responseBody);
+                return chiste.value;
+            }
+        }
+
+        public bool ChisteDuplicado(string chiste)
+        {
+            if (File.Exists(_filePath))
+            {
+                var chistes = File.ReadAllLines(_filePath);
+                foreach (var line in chistes)
+                {
+                    if (line.Equals(chiste, StringComparison.OrdinalIgnoreCase))
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        
+        public void GuardarChiste(string chiste)
+        {
+            int numeroChiste = Contador();
+            using (StreamWriter writer = new StreamWriter(_filePath, true))
+            {
+                writer.WriteLine($"{numeroChiste}. {chiste}");
+            }
+        }
+
+        private int Contador()
+        {
+            if (File.Exists(_filePath))
+            {
+                var chistes = File.ReadAllLines(_filePath);
+                return chistes.Length + 1;
+            }
+            return 1;
+        }
+
+        public void BuscarChiste(string palabraClave)
+        {
+            if (File.Exists(_filePath))
+            {
+                var chistes = File.ReadAllLines(_filePath);
+                bool encontrado = false;
+
+                Console.WriteLine("");
+
+                foreach (var chiste in chistes)
+                {
+                    if (chiste.IndexOf(palabraClave, StringComparison.OrdinalIgnoreCase) >=0)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Cyan;
+                        Console.WriteLine($" {chiste}");
+                        Console.ResetColor();
+                        encontrado = true;
+                    }
+                }
+
+                if (!encontrado)
+                {
+                    Console.WriteLine("No se encontraron chistes con la palabra clave " + palabraClave);
+                }
+            }
+            else
+            {
+                Console.WriteLine("El archivo no existe");
+            }
+        }
+    }
+}
---
+++ Proyecto_Chiste_Random/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoSemana6
+{
+    class Program
+    {
+        enum Menu
+        {
+            NuevoChiste,
+            BuscarChiste,
+            Salir
+        }
+
+        static async Task Main(string[] args)
+        {
+            string filePath = "Chiste.txt";
+            Chistes chisteRandom = new Chistes(filePath);
+            bool correr = true;
+
+            while (correr)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("");
+                Console.WriteLine("  _________________________");
+                Console.WriteLine("       Menu Principal      ");
+                Console.ResetColor();
+                Console.WriteLine("");
+                Console.WriteLine("  1. Nuevo Chiste");
+                Console.WriteLine("  2. Buscar Chiste");
+                Console.WriteLine("  3. Salir");
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(" _________________________");
+                Console.ResetColor();
+                Console.Write(" Ingrese una opcion: ");
+
+                string respuesta = Console.ReadLine();
+                Menu seleccionar;
+
+                switch (respuesta)
+                {
+                    case "1":
+                        seleccionar = Menu.NuevoChiste;
+                        break;
+                    case "2":
+                        seleccionar = Menu.BuscarChiste;
+                        break;
+                    case "3":
+                        seleccionar = Menu.Salir;
+                        break;
+                    default:
+                        Console.WriteLine(" Esta opcion no es valida");
+                        continue;
+                }
+
+                switch (seleccionar)
+                {
+                    case Menu.NuevoChiste:
+                        string chiste = await chisteRandom.ChisteAleatorio();
+                        if (!chisteRandom.ChisteDuplicado(chiste))
+                        {
+                            chisteRandom.GuardarChiste(chiste);
+                            Console.WriteLine(" Chiste guardado...");
+                        }
+                        else
+                        {
+                            Console.WriteLine(" El chiste ya existe en el archivo");
+                        }
+                        break;
+
+                    case Menu.BuscarChiste:
+                        Console.Write(" Ingrese una palabra clave: ");
+                        string resp = Console.ReadLine();
+                        chisteRandom.BuscarChiste(resp);
+                        break;
+
+                    case Menu.Salir:
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine(" Saliendo del programa.....");
+                        Console.ResetColor();
+                        await Task.Delay(1000);
+                        correr = false;
+                        break;
+                }
+
+            }
+        }
+    }
+}
